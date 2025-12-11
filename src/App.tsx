bb5afb45a5281762164a9359a7acd1ad9cb29e39
rlacodud <rlacodud0313@naver.com>
2025-12-12 02:08:11 +0900

    refactor: App 및 메인 진입점을 FSD 구조에 맞게 업데이트
    - App.tsx: Router Provider 사용
    - main.tsx: QueryClient Provider 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,18 @@
-import { BrowserRouter as Router } from "react-router-dom"
-import Header from "./components/Header.tsx"
-import Footer from "./components/Footer.tsx"
-import PostsManagerPage from "./pages/PostsManagerPage.tsx"
+import Footer from "./widgets/layout/ui/Footer"
+import Header from "./widgets/layout/ui/Header"
+import PostsManagerPage from "./pages/post/PostsManagerPage"
 
 const App = () => {
   return (
-    <Router>
-      <div className="flex flex-col min-h-screen">
-        <Header />
-        <main className="flex-grow container mx-auto px-4 py-8">
-          <PostsManagerPage />
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex-grow container mx-auto px-4 py-8">
+        <PostsManagerPage />
+      </main>
+      <Footer />
+    </div>
   )
 }
 
 export default App
+
