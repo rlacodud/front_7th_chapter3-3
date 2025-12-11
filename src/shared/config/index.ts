a3ad0044821c361296b157bb4325c097b51bce77
rlacodud <rlacodud0313@naver.com>
2025-12-12 03:33:16 +0900

    chore: api 설정 업데이트

--- src/shared/config/index.ts
+++ src/shared/config/index.ts
@@ -2,7 +2,5 @@
 // 앱 전체에서 사용되는 상수, 설정을 여기에 정의
 
 // ✅ 환경 변수에 따라 Base URL 설정
-const isDev = import.meta.env.DEV
-
-export const API_URL = isDev ? "/api" : "https://dummyjson.com"
-
+// 빌드 시 Vite가 import.meta.env를 정적으로 치환합니다
+export const API_URL = import.meta.env.MODE === "production" ? "https://dummyjson.com" : "/api"
